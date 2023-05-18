065d0c74c3890e8f5e1282d5f7c23ccb89e57d78
Rosalise <kapsophia@gmail.com>
2023-05-18 13:04:39 +0300

    Add list of places

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,39 +1,48 @@
 import { Injectable, OnInit } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/compat/firestore';
-import { collectionData, collection } from '@angular/fire/firestore'
-import {  deleteDoc, doc, getFirestore, setDoc } from 'firebase/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { collectionData, collection } from '@angular/fire/firestore';
+import { deleteDoc, doc, getFirestore, setDoc } from 'firebase/firestore';
 import { Observable, of } from 'rxjs';
 
 import { Place } from 'src/models/place';
 
-
 @Injectable({
   providedIn: 'root',
 })
-export class FavoritesService{
+export class FavoritesService {
   currentUser: any;
   userUid: any;
-  db = getFirestore()
-  
-  
+  db = getFirestore();
+
   constructor(private firestore: AngularFirestore) {
-    this.currentUser = JSON.parse(localStorage.getItem('user')!)
-    this.userUid = this.currentUser.uid;
+    this.currentUser = JSON.parse(localStorage.getItem('user')!);
+
+    if (this.currentUser !== null) {
+      this.userUid = this.currentUser.uid;
+    }
   }
 
   addToFavorites(place: any) {
-    const collectionRef = doc(this.db, 'users', this.userUid, 'favorites', place.placeId)
-    setDoc(collectionRef, place)
+    const collectionRef = doc(
+      this.db,
+      'users',
+      this.userUid,
+      'favorites',
+      place.placeId
+    );
+    setDoc(collectionRef, place);
   }
 
-  getAllFavorites(): Observable<Place[]>{
-    const userRef = doc(this.db, 'users', this.userUid)
+  getAllFavorites(): Observable<Place[]> {
+    const userRef = doc(this.db, 'users', this.userUid);
     const favoritesRef = collection(userRef, 'favorites');
-    return collectionData(favoritesRef, { idField: 'id' }) as Observable<Place[]>
+    return collectionData(favoritesRef, { idField: 'id' }) as Observable<
+      Place[]
+    >;
   }
 
-  deleteFavorite(placeId: string){
-    const docRef = doc(this.db, 'users', this.userUid, 'favorites', placeId)
+  deleteFavorite(placeId: string) {
+    const docRef = doc(this.db, 'users', this.userUid, 'favorites', placeId);
     deleteDoc(docRef);
   }
 }
