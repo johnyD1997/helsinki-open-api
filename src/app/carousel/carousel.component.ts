0dc2b4d99ed6f906ab7dd8913147e1cbba20a0da
Al-hassan Ahmed <ahmed.alhassan97@hotmail.com>
2023-05-18 10:04:57 +0300

    Added carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -18,14 +18,14 @@ export class CarouselComponent implements OnInit {
 
     this.images = [
       new ImageItem({ 
-        src: 'http://via.placeholder.com/640x360', 
-        thumb: 'http://via.placeholder.com/640x360'}),
+        src: 'https://images.unsplash.com/photo-1538332576228-eb5b4c4de6f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 
+        thumb: 'https://images.unsplash.com/photo-1538332576228-eb5b4c4de6f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'}),
         new ImageItem({ 
-          src: 'http://via.placeholder.com/640x360', 
-          thumb: 'http://via.placeholder.com/640x360'}),
+          src: 'https://images.unsplash.com/photo-1522885147691-06d859633fb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 
+          thumb: 'https://images.unsplash.com/photo-1522885147691-06d859633fb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'}),
           new ImageItem({ 
-            src: 'http://via.placeholder.com/640x360', 
-            thumb: 'http://via.placeholder.com/640x360'}),
+            src: 'https://images.unsplash.com/photo-1566111947159-0fcbf19648bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 
+            thumb: 'https://images.unsplash.com/photo-1566111947159-0fcbf19648bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'}),
 
         
     
